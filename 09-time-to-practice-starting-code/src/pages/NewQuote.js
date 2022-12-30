4100e2bf5c71935debcd810aee4f99771cd7c1b8
shannone <shannone426@gmail.com>
2022-12-30 10:21:12 -0500

    added useHistory hook to new quotes comp. added push method to allow page to close after new qoute also allows a back option unlike .replace()

--- 09-time-to-practice-starting-code/src/pages/NewQuote.js
+++ 09-time-to-practice-starting-code/src/pages/NewQuote.js
@@ -1,8 +1,13 @@
+import { useHistory } from "react-router-dom";  
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+const history = useHistory();
+
     const  addQuoteHandler = quoteData => {
         console.log(quoteData);
+//using .push allows for a go back option .replace does not
+        history.push('/quotes');
     };
     return (
         <QuoteForm onAddQuote={addQuoteHandler}/>
