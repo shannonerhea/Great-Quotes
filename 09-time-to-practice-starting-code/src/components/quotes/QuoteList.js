f4dfe29a005845e478bd38b9094a07c0079c5fc1
shannone <shannone426@gmail.com>
2022-12-30 11:58:22 -0500

    fixed bug in sort logic done ascending descending woot woot

--- 09-time-to-practice-starting-code/src/components/quotes/QuoteList.js
+++ 09-time-to-practice-starting-code/src/components/quotes/QuoteList.js
@@ -4,6 +4,18 @@ import { useHistory, useLocation } from "react-router-dom";
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
+
+//logic helper function for sorting
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
   const history = useHistory();
   const location = useLocation();
